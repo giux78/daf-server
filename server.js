2e69a73d8cc82ea8cb9ed9e1b5f3c02be0eab520
esterpantaleo <esterpantaleo@gmail.com>
2018-05-17 11:49:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+
+const server = Hapi.server({
+    port: 4000,
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        },
+	cors: {
+	    origin: ['*'],
+            credentials: true
+	}
+    }
+});
+
+const start = async () => {
+
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/menu.json',
+        handler: function (request, h) {
+            return h.file('menu.json');
+        }
+    });
+
+    server.route({
+	method: 'GET',
+	path: '/{city}/{file}',
+	handler: function (request, h) {
+	    return h.file(Path.join(request.params.city, request.params.file));
+	}
+    });
+
+    await server.start();
+
+    console.log('Server running at: ', server.info.uri);
+};
+
+start();
